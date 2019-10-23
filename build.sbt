e0a8eabc13d3cde5791408fb03a106a1f1d0fcf5
walid <walidsaidi.ws@gmail.com>
2019-10-23 14:42:53 +0200

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "untitled9"
+
+version := "0.1"
+
+scalaVersion := "2.11.0"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.3.1"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.3.1"
+libraryDependencies += "mysql" % "mysql-connector-java" % "5.1.44"
---
+++ src/main/scala/walid.scala
@@ -0,0 +1,73 @@
+import org.apache.spark.sql._
+import org.apache.spark.sql.types._
+
+object walid {
+
+  def main(args: Array[String]): Unit = {
+
+
+    val spark: SparkSession = SparkSession.builder()
+      .master("local[*]")
+      .appName("prog1").getOrCreate()
+
+    spark.sparkContext.setLogLevel("WARN")
+    import spark.implicits._
+
+    import org.apache.spark.sql.functions._
+    val studf = Seq(
+        Row(1, "u1", 5),
+        Row(1, "u2", 10),
+        Row(2, "u2", 20),
+        Row(3, "u1", 7),
+        Row(3, "u2", 15),
+        Row(4, "u2", 5),
+        Row(4, "u3", 10),
+        Row(5, "u1", 15),
+        Row(6, "u4", 10),
+        Row(7, "u5",9),
+        Row(8, "u6", 7))
+
+    //schema de la table source
+
+    val shemaTable =
+      StructType(
+
+        StructField("mois", IntegerType, true) ::
+        StructField("nom", StringType, true) ::
+        StructField("nbc", IntegerType, true) :: Nil)
+
+    val df = spark.createDataFrame(spark.sparkContext.parallelize(studf), shemaTable)
+    // df.show()
+    val  df1 = df.withColumnRenamed("nbc", "nombreConnexionACtuel")
+
+
+
+    df1.createOrReplaceTempView("table1")
+    df1.show()
+    val df2 =spark.sql(" select * from (select nom as nomp , mois+1 as moisp ,nombreConnexionACtuel as connexionprecedante from table1)" +
+                       "where moisp < (select max(mois+1) from table1)")
+    df2.show()
+    df2.createOrReplaceTempView("table2")
+
+    val resultat = spark.sql(" select * from (select t1.mois, t2.moisp , t1.nom ,t2.nomp,t1.nombreConnexionACtuel, " +
+                             "t2.connexionprecedante from" +
+                             " table1 t1 full join table2 t2 on t1.mois =t2.moisp and t1.nom = t2.nomp)   ")
+
+      .withColumn("connexionprecedante",
+        expr(" case when connexionprecedante IS NOT NULL then connexionprecedante else 0 end "))
+      .withColumn("nombreConnexionACtuel",
+        expr(" case when nombreConnexionACtuel IS NOT NULL then nombreConnexionACtuel else 0 end "))
+      .withColumn("nom",
+        expr("""case when nom IS NULL and nomp IS NOT NULL then nomp else nom end"""))
+      .withColumn("mois",
+        expr("""case when mois IS NULL and moisp IS NOT NULL then moisp else mois end"""))
+      .select("mois","nom","nombreConnexionACtuel","connexionprecedante")
+      .orderBy("mois")
+
+
+      resultat.show(50)
+
+  }
+
+
+}
